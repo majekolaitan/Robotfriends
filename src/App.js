db4a79318ece92a6719d4295bd37a76f8334ac92
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-08 02:05:31 +0400

    search component setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 const App = () => {
 	return(
 			<div>
 				<h1>Robotfriends</h1>
+				<SearchBox />
 				<CardList robots={robots}/>
 			</div>
 		);
