9ac32c43fbb5220518cc02493b74e45860987728
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-08 01:02:50 +0400

    code cleanup using destrustor

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,5 @@
 import React from 'react';
-const Card = (props) => {
-	const {name, email, id} = props;
+const Card = ({name, email, id}) => {
 	return (
 		<div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='Robot' src= {`https://robohash.org/${id}?size=200x200`} />
