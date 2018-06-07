c52c8dcec6809b8c1d52ada3545c1012673aeb59
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-08 00:34:31 +0400

    installed tachyons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
