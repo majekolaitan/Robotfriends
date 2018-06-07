f787ab77f89b235b5784b408fdfafcb63ca46c42
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-08 01:00:57 +0400

    code cleanup

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 const Card = (props) => {
+	const {name, email, id} = props;
 	return (
 		<div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='Robot' src= {`https://robohash.org/${props.id}?size=200x200`} />
+			<img alt='Robot' src= {`https://robohash.org/${id}?size=200x200`} />
 			<div>
-				<h2>{props.name}</h2>
-				<p>{props.email}</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
 		</div>
 	);
