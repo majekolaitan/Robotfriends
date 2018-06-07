1458dbfbed7d7404a383fdf70c2c07a45d68094e
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-08 00:58:39 +0400

    make robot photo dynamic

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Card = (props) => {
 	return (
 		<div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='Robot' src= 'https://robohash.org/test?size=200x200' />
+			<img alt='Robot' src= {`https://robohash.org/${props.id}?size=200x200`} />
 			<div>
 				<h2>{props.name}</h2>
 				<p>{props.email}</p>
