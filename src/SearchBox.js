61dd6d1ee36cea6ac08deb6f543f48a19d1d87e6
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-08 02:09:58 +0400

    style search box

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const SearchBox = () => {
 	return (
-			<div>
-				<input 
+			<div className='pa2'>
+				<input
+					className='pa3 ba b--green bg-lightest-blue'
 					type= 'search'  
 					placeholder='search robots' 
 				/>
