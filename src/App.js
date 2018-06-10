aa4fd5be2afa3a06d0500925962d71b038bd82bd
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-10 16:42:37 +0400

    errors when search. Any time you make your own methods on a componenet. use arrow functions

--- src/App.js
+++ src/App.js
@@ -12,8 +12,11 @@ class App extends Component  {
 		}
 	}
 
-	onSearchChange(event) {
-		console.log(event.target.value);
+	onSearchChange = (event) => {
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+		console.log(filteredRobots);
 	}
 
 	render() {
