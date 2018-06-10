b69d5029d83a51ff776560df7039bba7e40c69a0
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-10 10:59:42 +0400

    setting up state 'state is assigned a value but never used'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-	return(
+const state = {
+	robots: robots,
+	searchfield: ''
+}
+
+class App extends component  {
+	render() {
+		return(
 			<div className='tc'>
 				<h1>Robotfriends</h1>
 				<SearchBox />
 				<CardList robots={robots}/>
 			</div>
 		);
+	}
 }
 
 export default App;
