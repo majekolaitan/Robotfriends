bf4db2e9d22e10c98ae430ca4efeb675d79aa97f
majekolaitan@gmail.com <gVG9totEv22u>
2018-06-13 11:19:20 +0400

    return loading when list is empty

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component  {
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
-		.then(users => this.setState({robots: users}));
+		.then(users => []);
 	}
 
 	onSearchChange = (event) => {
@@ -26,13 +26,17 @@ class App extends Component  {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		})
-		return(
-			<div className='tc'>
+		if(this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		} else {
+			return(
+				<div className='tc'>
 				<h1 className='f1'>Robotfriends</h1>
 				<SearchBox searchChange = {this.onSearchChange}/>
 				<CardList robots={filteredRobots}/>
-			</div>
-		);
+				</div>
+				);
+		}
 	}
 }
 
